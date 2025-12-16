1f0855d267d668fd2a2f534008b3fce4a5971008
ernest <ernest@wpm.si>
2025-12-16 20:41:24 +0100

    Component

---
+++ frontend/src/components/Recipes/RecipeInstructionsCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    instructions: string | null
+}>();
+</script>
+
+<template>
+    <div v-if="instructions" class="bg-white rounded-2xl shadow-xl p-8">
+        <h2 class="text-2xl font-bold text-gray-900 mb-4">Instructions</h2>
+        <div class="prose max-w-none">
+            <p class="text-gray-700 leading-relaxed whitespace-pre-line">{{ instructions }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
