0f93406b82292a33e0e2885760e91a7e2863fb03
ernest <ernest@wpm.si>
2026-01-01 21:19:25 +0100

    Styles

--- frontend/src/components/Recipes/RecipeIngredientCard.vue
+++ frontend/src/components/Recipes/RecipeIngredientCard.vue
@@ -87,6 +87,10 @@ const addNewIngredientToForm = () => {
     });
 };
 
+const deleteIngredient = (index: number) => {
+    alert('DELETED');
+};
+
 const openAddModal = () => {
     const maxSortOrder = props.ingredients.length > 0
         ? Math.max(...props.ingredients.map(i => i.sort_order))
@@ -249,11 +253,19 @@ const addIngredient = () => {
         </template>
         <template #body>
             <div class="space-y-4">
-                <div v-for="(ingredient, index) in sortedFormData" 
+                <div v-for="(ingredient, index) in sortedFormData"
                      :key="ingredient.id ?? `new-${index}`"
                      :ref="(el) => { if (index === sortedFormData.length - 1) lastIngredientRef = el as HTMLElement }"
-                     class="p-3 sm:p-4 bg-gray-50 rounded-lg border border-gray-200 hover:border-gray-300 transition-colors">
-                    <div class="flex items-start gap-3 mb-3">
+                     class="p-3 sm:p-4 bg-gray-50 rounded-lg border border-gray-200 hover:border-gray-300 transition-colors relative">
+                    <button
+                        @click="deleteIngredient(index)"
+                        class="absolute top-0.25 right-0 p-2 rounded-lg active:scale-95 transition-transform duration-200"
+                    >
+                        <svg class="w-5 h-5 text-gray-500 hover:text-red-700 transition-all duration-200 hover:scale-150" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                        </svg>
+                    </button>
+                    <div class="flex items-start gap-3 mb-3 mt-1">
                         <!--                        <svg class="w-2 h-2 text-gray-600 flex-shrink-0 mt-3" fill="currentColor" viewBox="0 0 8 8">-->
                         <!--                            <circle cx="4" cy="4" r="3"/>-->
                         <!--                        </svg>-->
