a6b2cd73715b6bc44d59d6db82758898b2d30a15
ernest <ernestsmotlak@gmail.com>
2026-01-05 21:24:19 +0100

    Removed console log

--- frontend/src/components/Recipes/RecipeInstructionsCard.vue
+++ frontend/src/components/Recipes/RecipeInstructionsCard.vue
@@ -229,7 +229,6 @@ const toggleStep = (instruction: RecipeInstruction) => {
     axios.post(url)
         .then((response) => {
             const updatedRecipe = response.data.data;
-            console.log('recipe: ', response.data.data);
             emit('updatedRecipe', updatedRecipe);
         })
         .catch((error) => {
