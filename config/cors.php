2aaa1ce527a9edac75b551ff4e921b514d6bea4d
ernest <ernestsmotlak@gmail.com>
2026-01-07 23:11:32 +0100

    cors
    Signed-off-by: ernest <ernestsmotlak@gmail.com>

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,15 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    // read from env so prod + local both work
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
 
     'allowed_origins_patterns' => [],
 
@@ -32,7 +23,3 @@ return [
     'supports_credentials' => true,
 
 ];
-
-
-
-
