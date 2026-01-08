6bddf454816fb3fce38a8c02d430a68f522739e1
ernest <ernestsmotlak@gmail.com>
2026-01-08 22:25:24 +0100

    Work on component

--- frontend/src/pages/GroceryList/GroceryListPage.vue
+++ frontend/src/pages/GroceryList/GroceryListPage.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import DashboardLayout from "../../layouts/DashboardLayout.vue";
+import {ref} from "vue";
 
+const errorMessage = ref("");
+const groceryListData = ref({});
 </script>
 
 <template>
- GL page!
+    <DashboardLayout>
+        <div class="p-6 max-w-4xl mx-auto">
+            GL page!
+
+
+            <div v-if="errorMessage" class="bg-red-50 border border-red-200 rounded-lg p-4 mb-6">
+                <p class="text-red-600 font-medium">{{ errorMessage }}</p>
+            </div>
+
+            <div v-if="groceryListData">
+                Main
+            </div>
+
+            <div v-else-if="!errorMessage" class="text-center py-12">
+                <p class="text-gray-600 text-lg">Loading recipe...</p>
+            </div>
+        </div>
+    </DashboardLayout>
 </template>
 
 <style scoped>
