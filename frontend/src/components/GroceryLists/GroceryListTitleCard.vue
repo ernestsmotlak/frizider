13533d0306379ff75b57612412d19e7b2bbdd4c3
ernest <ernestsmotlak@gmail.com>
2026-01-16 20:29:13 +0100

    interface added

--- frontend/src/components/GroceryLists/GroceryListTitleCard.vue
+++ frontend/src/components/GroceryLists/GroceryListTitleCard.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+
+interface groceryListData {
+    id: number;
+    user_id: number;
+    name: string | null;
+    notes: string | null;
+    completed_at: string | null;
+    created_at: string | null;
+    updated_at: string | null;
+    deleted_at: string | null;
+    grocery_list_data: [];
+}
+
 const props = defineProps<{
     groceryListData: {
-        type: Object,
+        type: groceryListData,
         required: true,
     }
 }>()
@@ -9,7 +22,7 @@ const props = defineProps<{
 
 <template>
     Here GL main!
-    <pre class="bg-red-200">{{groceryListData}}</pre>
+    <pre class="bg-red-200">{{props.groceryListData}}</pre>
 </template>
 
 <style scoped>
