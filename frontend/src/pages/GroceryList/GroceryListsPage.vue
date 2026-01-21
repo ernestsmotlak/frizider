c41917a29d160abb230df9db2fa178300cec4bc9
ernest <ernestsmotlak@gmail.com>
2026-01-21 20:12:31 +0100

    Styles

--- frontend/src/pages/GroceryList/GroceryListsPage.vue
+++ frontend/src/pages/GroceryList/GroceryListsPage.vue
@@ -88,18 +88,14 @@ onUnmounted(() => {
             <div class="bg-gray-50 rounded-2xl border-2 border-gray-200">
                 <div class="px-4 pt-6 pb-4">
                     <div class="flex items-center justify-between mb-1">
-                        <!--                        <h1 class="text-3xl font-bold text-gray-900">Shopping Lists</h1>-->
-                        <div class="flex items-center gap-3 pb-4 mb-1 bg-red-200">
-                            <div class="flex-1">
-                                <h2 class="text-3xl sm:text-3xl font-bold tracking-tight text-gray-900">
-                                    Grocery Lists
-                                </h2>
-                                <p class="text-xs text-gray-500">
-                                    Long press to change status
-                                </p>
-                            </div>
+                        <div class="flex-1">
+                            <h2 class="text-3xl sm:text-3xl font-bold tracking-tight text-gray-900">
+                                Grocery Lists
+                            </h2>
+                            <p class="text-xs text-gray-500">
+                                Long press to change status
+                            </p>
                         </div>
-                        <div class="">here make a hr for fucks sake under the upper div</div>
                         <button
                             @click="handleAddGroceryList"
                             class="p-2 border-2 border-gray-200 bg-white/90 backdrop-blur-sm rounded-lg shadow-md hover:border-gray-300 hover:bg-white hover:shadow-xl hover:scale-110 active:scale-95 active:shadow-md transition-all duration-200"
@@ -110,6 +106,7 @@ onUnmounted(() => {
                             </svg>
                         </button>
                     </div>
+                    <hr class="border-gray-300 my-3" />
                     <p v-if="allRows > 0" class="text-sm text-gray-600">
                         {{ allRows }} shopping list{{ allRows !== 1 ? 's' : '' }}
                     </p>
