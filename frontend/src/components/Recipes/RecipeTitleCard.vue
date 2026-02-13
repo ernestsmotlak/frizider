18e108897b4e65d04483a449c974c55683ccee76
ernest <ernestsmotlak@gmail.com>
2026-02-13 19:42:00 +0100

    Cooking session button added

--- frontend/src/components/Recipes/RecipeTitleCard.vue
+++ frontend/src/components/Recipes/RecipeTitleCard.vue
@@ -147,6 +147,34 @@ const deleteRecipe = async () => {
             loadingStore.stop();
         });
 };
+
+const goCooking = async () => {
+    loadingStore.start();
+
+    if (!props.recipe.id) {
+        toastStore.show('error', 'There was a problem with the recipe.');
+        return;
+    }
+
+    const payload = {
+        recipe_id: props.recipe.id
+    };
+
+    const url = '/api/create-cooking-session/';
+    axios
+        .post(url, payload)
+        .then((result) => {
+            toastStore.show('success', 'Cooking session created.');
+            router.push("/cooking");
+        })
+        .catch((error) => {
+            toastStore.show('error', "A problem occured when creating a cooking session.");
+        })
+        .finally(() => {
+            loadingStore.stop();
+            closeMenu();
+        })
+}
 </script>
 
 <template>
@@ -180,6 +208,14 @@ const deleteRecipe = async () => {
                                   d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                         </svg>
                         Delete Recipe
+                    </button>
+                    <button @click.stop="goCooking"
+                            class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 flex items-center gap-2">
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                        </svg>
+                        Cook the recipe
                     </button>
                 </div>
             </div>
