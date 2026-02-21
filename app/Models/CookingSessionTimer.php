f430c1098abcbdd27bf66f975e7ab772a310d381
ernest <ernestsmotlak@gmail.com>
2026-02-22 00:06:11 +0100

    Removed soft deletes

--- app/Models/CookingSessionTimer.php
+++ app/Models/CookingSessionTimer.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class CookingSessionTimer extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $fillable = [
         'cooking_session_id',
