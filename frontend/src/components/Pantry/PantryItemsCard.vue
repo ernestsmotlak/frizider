7e8ebc7edaecd2691771016d75bbcd7fb8d2e616
ernest <ernestsmotlak@gmail.com>
2026-06-15 21:42:11 +0200

    Button hover styles

--- frontend/src/components/Pantry/PantryItemsCard.vue
+++ frontend/src/components/Pantry/PantryItemsCard.vue
@@ -315,7 +315,7 @@ const deleteItem = async (item: PantryItem) => {
                     <div class="relative flex-shrink-0 dropdown-container opacity-100" @click.stop>
                         <button
                             @click="toggleDropdown(item.id)"
-                            class="p-1.5 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded transition-colors opacity-100"
+                            class="p-1.5 text-gray-500 hover:text-gray-900 hover:bg-black/10 rounded transition-colors opacity-100"
                             title="More options"
                         >
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
